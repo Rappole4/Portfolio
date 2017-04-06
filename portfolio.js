9a677033e0e9d77543184b1158c65be3da99d7f8
Rob n Heidi <rappole4@yahoo.com>
2017-04-06 11:42:50 -0400

    chipping away at it

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#logo").fadeIn(7000);
+});
+$(document).ready(function(){
+    $(".links").fadeIn(7000);
+});
+$(document).ready(function(){
+    $(".icons a").fadeIn(7000);
+});
